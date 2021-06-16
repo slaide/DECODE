6225de4537ac1ed739f621d3a2ba9a892be9ead4
Lucas-Rapahel Müller <gitdev@lrm.photo>
2021-06-16 11:05:17 +0200

    Mod dataloader collate

--- decode/neuralfitter/utils/dataloader_customs.py
+++ decode/neuralfitter/utils/dataloader_customs.py
@@ -1,6 +1,6 @@
 import torch
 import torch.utils.data
-from torch._six import container_abcs, string_classes, int_classes
+# from torch._six import container_abcs
 
 import decode.generic
 
@@ -25,14 +25,6 @@ def smlm_collate(batch):
             storage = elem.storage()._new_shared(numel)
             out = elem.new(storage)
         return torch.stack(batch, 0, out=out)
-    elif isinstance(elem, container_abcs.Sequence):
-        # check to make sure that the elements in batch have consistent size
-        it = iter(batch)
-        elem_size = len(next(it))
-        if not all(len(elem) == elem_size for elem in it):
-            raise RuntimeError('each element in list of batch should be of equal size')
-        transposed = zip(*batch)
-        return [smlm_collate(samples) for samples in transposed]
     # END INSERT
     elif elem is None:
         return None
